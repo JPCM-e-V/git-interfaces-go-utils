b3bd35abbc2cfcf15eeeb4ee96bd8269c25875d9
rpkak <rpkak@users.noreply.github.com>
2022-01-30 12:34:29 +0100

    add some methods

---
+++ utils.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"unicode"
+)
+
+func PktLine(s string) string {
+	len_s := len(s)
+
+	if len_s > 65516 {
+		return PktLine("ERR To long response.")
+	}
+
+	for i := 0; i < len_s; i++ {
+		if s[i] > unicode.MaxASCII {
+			return PktLine("ERR Non ASCII character found.")
+		}
+	}
+	length := len_s + 5
+	return fmt.Sprintf("%04x%s\n", length, s)
+}
+
+func WriteGitProtocol(w http.ResponseWriter, lines []string) {
+	for _, line := range lines {
+		fmt.Fprint(w, PktLine(line))
+	}
+	fmt.Fprint(w, "0000")
+}
