23318608290a6c2f830d0fca60f7cc7929340132
rpkak <rpkak@users.noreply.github.com>
2022-01-31 15:24:54 +0100

    remove kv pairs

--- gitutils.go
+++ gitutils.go
@@ -6,29 +6,25 @@ import (
 	"unicode"
 )
 
-func PktLine(s string, value string) string {
-	if value != "" {
-		s = s + value
-	}
-
+func PktLine(s string) string {
 	len_s := len(s)
 
 	if len_s > 65515 {
-		return PktLine("ERR To long response.", "")
+		return PktLine("ERR To long response.")
 	}
 
 	for i := 0; i < len_s; i++ {
 		if s[i] > unicode.MaxASCII {
-			return PktLine("ERR Non ASCII character found.", "")
+			return PktLine("ERR Non ASCII character found.")
 		}
 	}
 	length := len_s + 5
 	return fmt.Sprintf("%04x%s\n", length, s)
 }
 
-func WriteGitProtocol(w http.ResponseWriter, lines map[string]string) {
-	for s, value := range lines {
-		fmt.Fprint(w, PktLine(s, value))
+func WriteGitProtocol(w http.ResponseWriter, lines []string) {
+	for _, s := range lines {
+		fmt.Fprint(w, PktLine(s))
 	}
 	fmt.Fprint(w, "0000")
 }
