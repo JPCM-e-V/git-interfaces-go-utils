f01e3ed3618a8171f6b2caa19e4a54f4067c96a0
rpkak <rpkak@users.noreply.github.com>
2022-02-05 14:04:39 +0100

    finish read protocol

--- gitutils.go
+++ gitutils.go
@@ -1,10 +1,18 @@
 package gitutils
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 	"unicode"
 )
+
+const FlushPkt = 0
+const DelimiterPkt = 1
+const ResponseEndPkt = 2
 
 func PktLine(s string) string {
 	len_s := len(s)
@@ -27,4 +35,42 @@ func WriteGitProtocol(w http.ResponseWriter, lines []string) {
 		fmt.Fprint(w, PktLine(s))
 	}
 	fmt.Fprint(w, "0000")
+}
+
+func ReadGitProtocol(r io.ReadCloser) ([]string, error) {
+	var lines []string
+
+	for {
+		b := make([]byte, 4)
+		n, err := r.Read(b)
+		if err == io.EOF {
+			break
+		} else if n != 4 {
+			return lines, errors.New("hex too short")
+		} else {
+			num, err0 := strconv.ParseUint(string(b), 16, 16)
+			if err0 != nil {
+				return lines, errors.New("wrong hex value")
+			} else if num >= 4 {
+				b_val := make([]byte, num-4)
+				n_val, err1 := r.Read(b_val)
+				if uint64(n_val) != num-4 || err1 == io.EOF {
+					return lines, errors.New("packet too short")
+				} else {
+					lines = append(lines, "p"+strings.TrimSuffix(string(b_val), "\n"))
+				}
+			} else if num == 3 {
+				return lines, errors.New("hex is 0003")
+			} else if num == FlushPkt {
+				lines = append(lines, "flush")
+			} else if num == DelimiterPkt {
+				lines = append(lines, "delimiter")
+			} else if num == ResponseEndPkt {
+				lines = append(lines, "responseend")
+			}
+		}
+	}
+
+	r.Close()
+	return lines, nil
 }
